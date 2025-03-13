12d85c6fc3724da5227bad5ff42d6fa8541cfd77
moriT958 <mkdeadg@gmail.com>
2025-03-13 18:28:55 +0900

    add: NoteRepositoryの作成

---
+++ note/note_test.go
@@ -0,0 +1,33 @@
+package note
+
+import (
+	"database/sql"
+	_ "modernc.org/sqlite"
+	"testing"
+)
+
+func TestSave(t *testing.T) {
+	db, err := sql.Open("sqlite", "../data.sqlite")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	repo, err := NewNoteRepository(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	note := Note{
+		ID:    1,
+		Title: "Test",
+		Path:  "./test/path",
+	}
+
+	id, err := repo.Save(note)
+	if err != nil {
+		t.Error(err)
+	}
+	if id != note.ID {
+		t.Error("idが違います")
+	}
+}
