62390afc3a2c4f5885904550fb79831c65c46111
moriT958 <morit958@icloud.com>
2025-05-02 22:10:58 +0900

    fix: load initial note files

--- file_operations.go
+++ file_operations.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/fs"
 	"os"
@@ -19,42 +18,26 @@ type note struct {
 	content string
 }
 
-// sample data
-// var notes []note = []note{
-// 	{title: "sample", content: "# sample0\n\n## hello\n\nthis is example0."},
-// 	{title: "sample", content: "# sample1\n\n## hello\n\nthis is example1."},
-// 	{title: "sample", content: "# sample2\n\n## hello\n\nthis is example2."},
-// 	{title: "sample", content: "# sample3\n\n## hello\n\nthis is example3."},
-// 	{title: "sample", content: "# sample4\n\n## hello\n\nthis is example4."},
-// 	{title: "sample", content: "# sample5\n\n## hello\n\nthis is example5."},
-// }
-
 const baseDir string = "./notes"
 
 func loadNoteFiles(baseDir string) ([]note, error) {
 	notes := make([]note, 0)
 
 	if err := filepath.Walk(baseDir, func(path string, info fs.FileInfo, err error) error {
-		if info.IsDir() {
-			return filepath.SkipDir
-		}
-
-		_, filename := filepath.Split(path)
-		filename = strings.TrimSuffix(filename, ".md")
-		parts := strings.Split(filename, "-")
-		if len(parts) < 4 {
-			return errors.New(fmt.Sprintf("unexpected filename format: %s", filename))
-		}
-
-		title := strings.Join(parts[:len(parts)-3], "-")
-		var content []byte
-		fp, err := os.Open(path)
 		if err != nil {
 			return err
 		}
-		defer fp.Close()
-		fp.Read(content)
+		if info.IsDir() {
+			return nil
+		}
 
+		content, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		_, filename := filepath.Split(path)
+		title := getTitleFromFilename(filename)
 		notes = append(notes, note{title, string(content)})
 
 		return nil
@@ -63,6 +46,14 @@ func loadNoteFiles(baseDir string) ([]note, error) {
 	}
 
 	return notes, nil
+}
+
+func getTitleFromFilename(filename string) string {
+	filename = strings.TrimSuffix(filename, ".md")
+	parts := strings.Split(filename, "-")
+
+	title := strings.Join(parts[:len(parts)-3], "-")
+	return title
 }
 
 func createNewNoteFile(title string) tea.Cmd {
