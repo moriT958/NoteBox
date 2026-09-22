40081b4b9bfac7eafc12a2991d3202bc2199af6b
Morita <morit958@icloud.com>
2026-09-22 22:35:42 +0900

    test(core): Use `t.Helper` at `newStubBox`

--- internal/note/service_test.go
+++ internal/note/service_test.go
@@ -14,6 +14,7 @@ type stubBox struct {
 var _ noteBox = (*stubBox)(nil)
 
 func newStubBox(t *testing.T, name string) *stubBox {
+	t.Helper()
 	dir := t.TempDir()
 	if err := os.MkdirAll(path.Join(dir, name), 0755); err != nil {
 		t.Fatalf("failed to create stub box: %v", err)
